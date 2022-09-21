285f9c0002220b2a2605886d0dc904e974c5441c
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-09-21 00:52:06 -0700

    testing

---
+++ src/main/java/com/omar/data_structures/Main.java
@@ -0,0 +1,13 @@
+package com.omar.data_structures;
+
+/**
+ * Package: com.omar.data_structures
+ * Class: Main.java
+ * Created by omar on 9/21/22
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
