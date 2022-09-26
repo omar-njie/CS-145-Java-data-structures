4a1439900771c26956118715b867083e88446d98
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-09-26 00:26:12 -0700

    updated

--- src/main/java/com/omar/data_structures/assignments/Main.java
+++ src/main/java/com/omar/data_structures/assignments/Main.java
@@ -4,37 +4,40 @@ import java.util.Random;
 
 /**
  * @author Omar
- * @version 9.21.22
+ * @version 9.26.22
  */
 public class Main {
 
-    public int a;
-    public char b;
-    private String c;
-
-    public Main() {
-        c = "Hello";
-    }
-
-    public Main(char x) {
-        c = "Hello" + x;
-        b = x;
-    }
-
-    public void print() {
-        System.out.println(c);
-    }
-
-
     public static void main(String[] args) {
-       // Main m = new Main();
-       // m.print();
+        Bird[] birds = new Bird[6];
+        birds[0] = new Bird(5, 2, 3, "Eagle");
+        birds[1] = new Bird(3, 3, 3, "Hawk");
+        birds[2] = new Bird(1, 1, 1, "Finch");
+        birds[3] = new Bird(5, 5, 5, "Sparrow");
+        birds[4] = new Bird(1, 3, 0, "Humming Bird");
+        birds[5] = new Bird();
 
-        // generate a random number between -6 and 6
-        Random r = new Random();
-        int x = r.nextInt(12) - 6;
-        int y = r.nextInt(12) - 6;
-        int z = r.nextInt(12) - 6;
-        System.out.println("x: " + x + ", y: " + y + ", z: " + z);
+        int touching = 0;
+        int round = 0;
+        while (touching < 4) {
+            round++;
+            System.out.println("Round " + round);
+            for (Bird bird : birds) {
+                bird.move();
+            }
+            for (int i = 0; i < birds.length; i++) { // O(n^2)
+                for (int j = i + 1; j < birds.length; j++) {
+                    if (birds[i].touching(birds[j])) {
+                        System.out.println("TOUCH : " + birds[i].getName() + " vs " + birds[j].getName());
+                        touching++;
+                    }
+                }
+            }
+            for (Bird bird : birds) {
+                System.out.println(bird);
+            }
+            System.out.println("[####################################]");
+        }
+        System.out.println("There was a total of " + round + " rounds to get to 4 touches.");
     }
 }
