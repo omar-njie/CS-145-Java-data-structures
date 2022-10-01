830667ca9aa34c011d19df55d43b362949508831
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-10-01 04:55:28 -0700

    updated

--- src/main/java/com/omar/data_structures/assignments/assignment_2/JavaCritter/Giant.java
+++ src/main/java/com/omar/data_structures/assignments/assignment_2/JavaCritter/Giant.java
@@ -4,6 +4,13 @@ package com.omar.data_structures.assignments.assignment_2.JavaCritter;
  * @author Omar
  * @version 9/29/22
  */
-public class Giant {
+public class Giant extends Critter {
+
+    private int CowardType;
+    private int WarriorType;
+    public Giant() {
+        // determines if the Giant is a CowardType(25% chance) or a WarriorType(75% chance).
+        
+    }
 
 }
