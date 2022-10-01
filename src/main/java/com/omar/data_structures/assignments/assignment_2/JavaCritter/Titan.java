3a3493528010b989eae17ea9dbdeca80e634d49f
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-10-01 04:55:51 -0700

    updated

--- src/main/java/com/omar/data_structures/assignments/assignment_2/JavaCritter/Titan.java
+++ src/main/java/com/omar/data_structures/assignments/assignment_2/JavaCritter/Titan.java
@@ -4,10 +4,11 @@ package com.omar.data_structures.assignments.assignment_2.JavaCritter;
  * @author Omar
  * @version 9/29/22
  */
-public class Titan {
+public class Titan extends Critter {
 
     public Titan() {
 
     }
+
 
 }
