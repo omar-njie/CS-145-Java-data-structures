2c7ef12cb69637b9d6beb6adb8e7162596050f9b
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-10-04 02:11:41 -0700

    updated

--- src/main/java/com/omar/data_structures/assignments/assignment_2/JavaCritter/Titan.java
+++ src/main/java/com/omar/data_structures/assignments/assignment_2/JavaCritter/Titan.java
@@ -8,26 +8,6 @@ import java.awt.*;
  */
 public class Titan extends Giant {
 
-    /*
-    The titan class is an extension/subclass of the Giant class.   As such it should only have the
-    following 3 methods.  Also,  it should have ZERO fields, since it is a subclass of Giant.
-        Titan()
-        getColor
-            Color.BLACK
-                When saying “fee” or "FEE"
-            COLOR.GRAY
-                When saying “fie” or “fum” or the capitalized versions.
-            Color.WHITE
-                When saying “foe” or "FOE".
-            Note that this replaces the color from the Giant Base class.
-
-       Note that the Titan Class should NOT have any other methods, they will be inherited that
-       method from the Giant class.
-       VERY IMPORTANT
-            You should NOT have any private variables in Titan, your goal with this class is
-            to have the parent/(Giant) class do all the hard work, and then this class will look
-            at the data, and return the new/modified result. You will need to use your superpower here!
-    */
     public Titan() {}
 
     @Override
@@ -45,16 +25,4 @@ public class Titan extends Giant {
         }
     }
 
-    public static void main(String[] args) {
-        Giant g = new Giant();
-        Titan t = new Titan();
-        for (int i = 0; i < 10; i++) {
-            System.out.println(g);
-            System.out.println(t);
-            System.out.println(g.getColor());
-            System.out.println(t.getColor());
-            System.out.println("------------------");
-        }
-
-    }
 }
