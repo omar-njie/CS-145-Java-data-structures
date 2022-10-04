044c6f990b532f12bed7de030bcca589045f0cd3
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-10-04 02:11:12 -0700

    updated

--- src/main/java/com/omar/data_structures/assignments/assignment_2/JavaCritter/Bear.java
+++ src/main/java/com/omar/data_structures/assignments/assignment_2/JavaCritter/Bear.java
@@ -4,24 +4,36 @@ import java.awt.*;
 import java.util.Random;
 
 /**
+ * <h3>
+ *     The Bear class is a subclass of the Critter class.
+ *     It represents a Bear inside a critter simulation.
+ *     It checks if the bear is a normal or a polar bear.
+ * </h3>
+ * @see Critter
  * @author Omar
  * @version 9/29/22
  */
 public class Bear extends Critter {
 
-    private boolean is_normal_bear = true;
+    private final boolean is_normal_bear;
     private int move_count;
 
+    /**
+     * Decide whether the bear is a normal bear or a polar bear
+     * There is a 50% that the bear could be normal or polar
+     */
     public Bear() {
         Random rand = new Random();
         is_normal_bear = rand.nextInt(100) < 50;
         move_count = 0;
     }
 
+
     @Override
     public Color getColor() {
         return !(is_normal_bear) ? Color.WHITE : Color.BLACK;
     }
+
 
     @Override
     public String toString() {
@@ -31,6 +43,7 @@ public class Bear extends Critter {
             return "\\";
         }
     }
+
 
     @Override
     public Action getMove(CritterInfo info) {
--- src/main/java/com/omar/learn/RandomChance.java
+++ src/main/java/com/omar/learn/RandomChance.java
@@ -24,6 +24,19 @@ public class RandomChance {
         }
         System.out.println("Random numbers with no duplicates = " + set);
 
+        /*        Set<Color> colors = new LinkedHashSet<>();
+        while (colors.size() < 5) {
+            int random_color = rand.nextInt(5) + 1;
+            colors.add(switch (random_color) {
+                case 1 -> Color.RED;
+                case 2 -> Color.GREEN;
+                case 3 -> Color.YELLOW;
+                case 4 -> Color.BLUE;
+                case 5 -> Color.PINK;
+                default -> Color.BLACK;
+            });
+        }
+        return colors.iterator().next();*/
     }
 
 }
