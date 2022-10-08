6974b49d90f48f265fa59b2db59d31a65f1b9846
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-10-08 03:47:08 -0700

    updated

---
+++ src/main/java/com/omar/data_structures/assignments/short_assignments/CS145ArrayListPractice.java
@@ -0,0 +1,77 @@
+package com.omar.data_structures.assignments.short_assignments;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * List Practice
+ * @author Omar
+ * @version 10/8/22
+ */
+public class CS145ArrayListPractice {
+
+    public static void main(String[] args) {
+
+        List<String> myList = new ArrayList<>();
+
+        myList.add("Four");
+        myList.add("score");
+        myList.add("and");
+        myList.add("seven");
+        myList.add("years");
+        myList.add("ago.");
+        System.out.print("Before: ");
+        System.out.println(myList);
+
+        swapPairs(myList);
+
+        System.out.print("After:  ");
+        System.out.println(myList);
+        // Round 2 - Odd
+        System.out.println();
+        System.out.println("***********");
+        System.out.println();
+
+        myList.clear();
+
+        myList.add("1");
+        myList.add("2");
+        myList.add("3");
+        myList.add("4");
+        myList.add("5");
+        myList.add("6");
+        myList.add("7");
+        myList.add("8");
+        myList.add("9");
+        myList.add("10");
+        myList.add("11");
+        System.out.print("Before: ");
+        System.out.println(myList);
+
+        swapPairs(myList);
+
+        System.out.print("After:  ");
+        System.out.println(myList);
+    }
+
+    // Round 1 - Even
+    private static void swapPairs(List<String> my_list) {
+        if (my_list.size() % 2 == 0) {
+            int x = 0;
+            while (x < my_list.size()) {
+                String _temp = my_list.get(x);
+                my_list.set(x, my_list.get(x + 1));
+                my_list.set(x + 1, _temp);
+                x += 2;
+            }
+        } else {
+            /*
+             calling the swapPairs method recursively,
+             this will make the last element of the list remain the same
+             NOTE: the swapping is always done in the first portion of the code.
+            */
+            swapPairs(my_list.subList(0, my_list.size() - 1));
+        }
+    }
+
+}
