098ebe89bb54bb4242adc4132d545e99121a2b9b
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-10-20 18:20:13 -0700

    updated

--- src/main/java/com/omar/data_structures/assignments/assignment_3/LevenshteinFinder.java
+++ src/main/java/com/omar/data_structures/assignments/assignment_3/LevenshteinFinder.java
@@ -11,9 +11,8 @@ import java.util.*;
  */
 public class LevenshteinFinder {
 
-    private final String from, to;
     private final Map<String, Set<String>> words_list;
-    private int distance = -1;
+    private final int distance;
     private List<String> path;
 
     /**
@@ -26,15 +25,11 @@ public class LevenshteinFinder {
      * @throws IllegalArgumentException if the two entered words from the use aren't the same length.
      */
     public LevenshteinFinder(String from, String to, Set<String> words_list) {
-        this.from = from;
-        this.to = to;
-
-        if (this.from.length() != this.to.length()) {
+        if (from.length() != to.length())
             throw new IllegalArgumentException(
-                    "The two words must be the same length"
-            );
-        }
+                    "The two words must be the same length");
 
+        this.path = new ArrayList<>();
         this.words_list = new TreeMap<>();
         for (String word : words_list) {
             if (word.length() == from.length()) {
@@ -44,25 +39,18 @@ public class LevenshteinFinder {
 
         for (String word : this.words_list.keySet()) {
             for (String word_2 : this.words_list.keySet()) {
-                if (is_neighbor(word, word_2)) {
+                int is_neighbor = differentLetters(word, word_2);
+                if (is_neighbor == 1) {
                     this.words_list.get(word).add(word_2);
                     this.words_list.get(word_2).add(word);
                 }
             }
         }
+
         this.distance = findDistance(from, to);
         findPath(from, to);
     }
 
-    private boolean is_neighbor(String word, String word_2) {
-        int count = 0;
-        for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) != word_2.charAt(i)) {
-                count++;
-            }
-        }
-        return count == 1;
-    }
 
     /**
      * @return distance
@@ -71,38 +59,37 @@ public class LevenshteinFinder {
         return this.distance;
     }
 
+
     /**
      * @return No path if the distance is -1, & the delimiter "->" + path
      * NOTE: the first word before the delimiter was entered by the user.
      * @see String#join(CharSequence, Iterable)
      */
     public String getPath() {
-        if (this.distance == -1) {
+        if (this.distance == -1)
             return "There is no path";
-        }
-        return String.join("->", path); // join two strings with the delimiter "->"
+        return String.join("->", this.path); // join two strings with the delimiter "->"
     }
 
-    // find two words
+
     private int findDistance(String from, String to) {
-        Set<String> set_1 = new TreeSet<>(Set.of());
-        Set<String> set_2 = new TreeSet<>(Set.of(from));
-        int counter = 0;
+        Set<String> set_1 = new TreeSet<>();
+        Set<String> set_2 = new TreeSet<>();
         set_2.add(from);
 
+        int counter = 0;
         while (set_1.size() != set_2.size() && !set_2.contains(to)) {
             set_1.addAll(set_2);
             set_2.clear();
             for (String word : set_1) {
-                set_2.addAll(words_list.get(word));
+                set_2.addAll(this.words_list.get(word));
                 set_2.add(word);
             }
             counter++;
         }
 
-        if (set_2.contains(to))
-            return counter;
-        return -1;
+        // if the set_2 contains the "to word" return counter else return -1
+        return set_2.contains(to) ? counter : -1;
     }
 
     private void findPath(String from, String to) {
@@ -114,13 +101,12 @@ public class LevenshteinFinder {
         }
         this.path.add(from);
 
-        if (this.distance == 0) {
+        if (this.distance == 0)
             return;
-        }
 
         for (int k = distance - 1; k > 0; k--) {
             String last = this.path.get(path.size() - 1);
-            for (String word : words_list.get(last)) {
+            for (String word : this.words_list.get(last)) {
                 if (findDistance(word, to) == k) {
                     this.path.add(word);
                     break;
