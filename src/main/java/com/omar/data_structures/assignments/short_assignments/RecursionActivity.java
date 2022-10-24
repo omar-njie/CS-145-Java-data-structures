6e2f4663b0674e5b103f66a8df79b42fa5197389
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-10-24 02:08:09 -0700

    created rca file

---
+++ src/main/java/com/omar/data_structures/assignments/short_assignments/RecursionActivity.java
@@ -0,0 +1,40 @@
+package com.omar.data_structures.assignments.short_assignments;
+
+/**
+ * Recursion Practice Activity.
+ *
+ * @author Omar
+ * @version 10.24.22
+ */
+public class RecursionActivity {
+
+    public static void main(String[] args) {
+        System.out.print(starString(2) + "\n");
+        System.out.println(evenDigits(345));
+    }
+
+
+    /**
+     * @param n the number of stars to print.
+     * @return a string of n stars.
+     * @throws IllegalArgumentException if n is less than 0.
+     */
+    static String starString(int n) {
+        if (n < 0) throw new IllegalArgumentException();
+        if (n == 0) return "*";
+        return starString(n - 1) + starString(n - 1);
+    }
+
+
+    /**
+     * @param n number to check.
+     * @return the number of even digits in n.
+     */
+    static int evenDigits(int n) {
+        if (n == 0) return 0;
+        if (n % 2 == 0)
+            return n % 10 + 10 * evenDigits(n / 10);
+        else
+            return evenDigits(n / 10);
+    }
+}
