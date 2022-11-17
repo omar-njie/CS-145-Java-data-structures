c84bef0a8d585c16a4d74da88238359df7fef005
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-11-17 00:52:06 -0800

    updated

--- src/main/java/com/omar/data_structures/assignments/assignment_5/CardLinkedList.java
+++ src/main/java/com/omar/data_structures/assignments/assignment_5/CardLinkedList.java
@@ -1,109 +1,216 @@
 package com.omar.data_structures.assignments.assignment_5;
 
-import java.util.LinkedList;
+import com.omar.learn.generics.Car;
+import com.omar.learn.implement_collections.linked_lists.ListNodeEx;
+
+import java.util.Random;
+import java.util.Stack;
+
 /**
  * @author Omar
  * @version 11/11/22
  */
 public class CardLinkedList implements CardList {
 
-    /**
-     * This method should return the current number of elements in the list.
-     *
-     * @return the size of the list as an integer
-     */
+    private CardNode<Card> front;
+    private int size;
+
+
+    public CardLinkedList() {
+        front = null;
+        size = 0;
+    }
+
+    private CardNode<Card> nodeAt(int index) {
+        CardNode<Card> current = front;
+        for (int i = 0; i < index; i++)
+            current = current.next;
+        return current;
+    }
+
+    @Override
+    public String toString() {
+        if (front == null)
+            return "[0 | " + this.size() + "]";
+        StringBuilder sb = new StringBuilder();
+        sb.append("[0 | ");
+        CardNode<Card> current = front;
+        while (current != null) {
+            sb.append(current.data.toString());
+            if (current.next != null)
+                sb.append(",");
+            current = current.next;
+        }
+        sb.append(" | ").append(this.size()).append("]");
+        return sb.toString();
+    }
+
     public int size() {
-        return 0;
+        return size;
     }
 
-    /**
-     * This method should add a card to the end of the list in the first available spot.
-     *
-     * @param c the Card object to be added.
-     */
     public void add(Card c) {
-
+        if (front == null) {
+            front = new CardNode<Card>(c);
+        } else {
+            CardNode<Card> current = front;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = new CardNode<Card>(c);
+        }
+        size++;
     }
 
-    /**
-     * This method should add a card to the indicated location
-     * sliding all other elements over one.
-     *
-     * @param loc the desired index of the card to be added.
-     * @param c   the Card object to be added.
-     * @throws IndexOutOfBoundsException if the loc is outside the current list.
-     */
-    public void add(int loc, Card c) {
-
+    public void add(int l, Card c) {
+        check_index(l);
+        if (l == 0) {
+            front = new CardNode<Card>(c, front);
+        } else {
+            CardNode<Card> current = nodeAt(l - 1);
+            current.next = new CardNode<Card>(c, current.next);
+        }
+        size++;
     }
 
-    /**
-     * This method should remove the last element from the list.
-     *
-     * @return The card object removed from the list.
-     * @throws IndexOutOfBoundsException if the list is empty.
-     */
-    public Card remove() {
-        return null;
+
+   public Card remove() {
+       if (front == null)
+           throw new IndexOutOfBoundsException();
+       Card result = front.data;
+       front = front.next;
+       size--;
+       return result;
+   }
+
+    public Card remove(int j) {
+        check_index(j);
+        Card result;
+        if (j == 0) {
+            result = front.data;
+            front = front.next;
+        } else {
+            CardNode<Card> current = nodeAt(j - 1);
+            result = current.next.data;
+            current.next = current.next.next;
+        }
+        size--;
+        return result;
     }
 
-    /**
-     * This method should remove the identified card from the list and return it.
-     *
-     * @param loc the index of the card to be removed.
-     * @return The card object removed from the list.
-     * @throws IndexOutOfBoundsException if the loc is outside the current array size.
-     */
-    public Card remove(int loc) {
-        return null;
+    public Card get(int x) {
+        check_index(x);
+        return nodeAt(x).data;
     }
 
-    /**
-     * This method should return the i'th last element from the list.
-     *
-     * @param i The index of the desired card.
-     * @return The card object located in index x from the list.
-     * @throws IndexOutOfBoundsException if the i value is outside the range of the list
-     */
-    public Card get(int i) {
-        return null;
+    public int indexOf(Card x) {
+        CardNode<Card> current = front;
+        for (int i = 0; i < size; i++) {
+            if (current.data.equals(x))
+                return i;
+            current = current.next;
+        }
+        return -1;
     }
 
-    /**
-     * Returns the index of specified Card object
-     *
-     * @param c Card object
-     * @return index of Card otherwise -1 if not found
-     */
-    public int indexOf(Card c) {
-        return 0;
-    }
-
-    /**
-     * Sort the items in the list from smallest to largest
-     */
     public void sort() {
+        if (size <= 1)
+            return;
+        CardLinkedList left = new CardLinkedList();
+        CardLinkedList right = new CardLinkedList();
+        int half = size / 2;
+        for (int i = 0; i < half; i++)
+            left.add(this.remove());
+        while (size > 0)
+            right.add(this.remove());
+        left.sort();
+        right.sort();
+        merge(left, right);
 
     }
 
-    /**
-     * Apply a weak shuffle algorithm to rearrange the items in the list.
-     */
+    private void sort(CardLinkedList x) {
+        if (x.size <= 1)
+            return;
+        CardLinkedList left = new CardLinkedList();
+        CardLinkedList right = new CardLinkedList();
+        int half = x.size / 2;
+        for (int i = 0; i < half; i++)
+            left.add(x.remove());
+        while (x.size > 0)
+            right.add(x.remove());
+        sort(left);
+        sort(right);
+        merge(left, right);
+    }
+
+    private void merge(CardLinkedList left, CardLinkedList right) {
+        while (left.size() + right.size() > 0) {
+            if (left.size() == 0)
+                this.add(right.remove());
+            else if (right.size() == 0)
+                this.add(left.remove());
+            else if (left.front.data.compareTo(right.front.data) < 0)
+                this.add(left.remove());
+            else
+                this.add(right.remove());
+        }
+    }
+
+
     public void shuffle() {
-
+        Random rand = new Random();
+        for (int i = 0; i < size * 5; i++) {
+            int a = rand.nextInt(size);
+            int b = rand.nextInt(size);
+            swap(a, b);
+        }
     }
 
-    /**
-     * Empty the list of all items.
-     */
     public void clear() {
-
+        front = null;
+        size = 0;
     }
 
-    /**
-     * Reverse the list from the first to last element.
-     */
-    public void reverse() {
+    private void swap(int a, int b) {
+        CardNode<Card> current = front;
+        CardNode<Card> current2 = front;
+        for (int i = 0; i < a; i++)
+            current = current.next;
+        for (int i = 0; i < b; i++)
+            current2 = current2.next;
+        Card temp = current.data;
+        current.data = current2.data;
+        current2.data = temp;
+    }
 
+
+    public void reverse() {
+        Stack<Card> stack = new Stack<Card>();
+        while (this.size > 0)
+            stack.push(this.remove());
+        while (!(stack.isEmpty()))
+            this.add(stack.pop());
+    }
+
+    private void check_index(int index) {
+        if (index < 0 || index > this.size)
+            throw new IndexOutOfBoundsException("Index" + index);
+    }
+
+    private static class CardNode<E> {
+        public E data;
+        public CardNode<E> next;
+
+
+        public CardNode(E data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        public CardNode(E data, CardNode<E> next) {
+            this.data = data;
+            this.next = next;
+        }
     }
 }
