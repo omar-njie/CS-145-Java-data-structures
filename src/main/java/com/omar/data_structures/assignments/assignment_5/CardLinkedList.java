2c5ed8e3d312fe2bc6cafb37757ca7836d013cf3
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-11-17 23:09:32 -0800

    updated

--- src/main/java/com/omar/data_structures/assignments/assignment_5/CardLinkedList.java
+++ src/main/java/com/omar/data_structures/assignments/assignment_5/CardLinkedList.java
@@ -18,13 +18,112 @@ public class CardLinkedList implements CardList {
         this.size = 0;
     }
 
-    private CardNode<Card> nodeAt(int index) {
+
+
+
+    public int size() {
+        return this.size;
+    }
+
+    public void add(Card c) {
+        if (front == null) {
+            front = new CardNode<Card>(c);
+        } else {
+            CardNode<Card> current = front;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = new CardNode<Card>(c);
+        }
+        size += 1;
+    }
+
+    public void add(int l, Card c) {
+        check_index_pos(l);
+        if (l == 0) {
+            front = new CardNode<Card>(c, front);
+        } else {
+            CardNode<Card> current = nodeAt(l - 1);
+            current.next = new CardNode<Card>(c, current.next);
+        }
+        size += 1;
+    }
+
+
+   public Card remove() {
+       if (front == null)
+           throw new IndexOutOfBoundsException("The list is empty");
+       CardNode<Card> current = front;
+       CardNode<Card> behind = front;
+       while (current.next != null) {
+           behind = current;
+           current = current.next;
+       }
+       Card c = current.item;
+       behind.next = null;
+       size -= 1;
+       return c;
+
+   }
+
+
+    public Card remove(int j) {
+        check_element_index(j);
+        if (j == 0) {
+            CardNode<Card> current = front;
+            front = front.next;
+            size -= 1;
+            return current.item;
+        }
+        CardNode<Card> current = nodeAt(j - 1);
+        CardNode<Card> temp = current.next;
+        current.next = current.next.next;
+        size -= 1;
+        return temp.item;
+    }
+
+    public Card get(int x) {
+        check_element_index(x);
+        return nodeAt(x).item;
+    }
+
+    public int indexOf(Card x) {
         CardNode<Card> current = front;
-        for (int i = 0; i < index; i++) {
+        for (int i = 0; i < this.size; i++) {
+            if (current.item.equals(x))
+                return i;
             current = current.next;
         }
-        return current;
+        return -1;
     }
+
+    public void sort() {
+        this.sort(this);
+    }
+
+    public void clear() {
+        front = null;
+        this.size = 0;
+    }
+
+    public void shuffle() {
+        Random rand = new Random();
+        for (int i = 0; i < size * 5; i++) {
+            int a = rand.nextInt(size);
+            int b = rand.nextInt(size);
+            swap(a, b);
+        }
+    }
+
+
+    public void reverse() {
+        Stack<Card> stack = new Stack<Card>();
+        while (this.size > 0)
+            stack.push(this.remove(0));
+        while (!(stack.isEmpty()))
+            this.add(stack.pop());
+    }
+
 
     @Override
     public String toString() {
@@ -43,148 +142,6 @@ public class CardLinkedList implements CardList {
         return sb.toString();
     }
 
-    public int size() {
-        return this.size;
-    }
-
-    public void add(Card c) {
-        if (front == null) {
-            front = new CardNode<Card>(c);
-        } else {
-            CardNode<Card> current = front;
-            while (current.next != null) {
-                current = current.next;
-            }
-            current.next = new CardNode<Card>(c);
-        }
-        size++;
-    }
-
-    public void add(int l, Card c) {
-        check_index(l);
-        if (l == 0) {
-            front = new CardNode<Card>(c, front);
-        } else {
-            CardNode<Card> current = nodeAt(l - 1);
-            current.next = new CardNode<Card>(c, current.next);
-        }
-        size++;
-    }
-
-
-   public Card remove() {
-        /*if (front == null)
-            return null;
-        CardNode<Card> current = front;
-        front = front.next;
-        size--;
-        return current.item;*/
-       if (front == null)
-           throw new IndexOutOfBoundsException("The list is empty");
-       CardNode<Card> temp = front;
-       if (size == 1) {
-           Card c = front.item;
-           clear();
-           return c;
-       } else if (size == 2) {
-           Card c = temp.next.item;
-           temp.next = null;
-           size--;
-           return c;
-       } else {
-           while (temp.next.next != null) {
-               temp = temp.next;
-           }
-           Card c = temp.next.item;
-           temp.next = null;
-           size--;
-           return c;
-       }
-   }
-
-    /*public Card remove(int j) {
-        check_index(j);
-        if (j == 0) {
-            CardNode<Card> current = front;
-            front = front.next;
-            size--;
-            return current.item;
-        } else {
-            CardNode<Card> current = nodeAt(j - 1);
-            CardNode<Card> temp = current.next;
-            current.next = current.next.next;
-            size--;
-            return temp.item;
-        }
-    }*/
-
-    public Card remove(int loc) {
-        if (front == null) {
-            throw new IndexOutOfBoundsException("The list is empty");
-        } else if (loc < 0) {
-            throw new IndexOutOfBoundsException("The index is negative");
-        } else if (loc > size) {
-            throw new IndexOutOfBoundsException("The index is greater than the size of the list");
-        } else if (loc == size) {
-            return remove();
-        } else if (loc == 0) {
-            Card c = front.item;
-            front = front.next;
-            size--;
-            return c;
-        } else {
-            CardNode<Card> current = front;
-            for (int i = 0; i < loc; i++) {
-                current = current.next;
-            }
-            Card c = current.next.item;
-            CardNode<Card> temp = current.next.next;
-            current.next = temp;
-            size--;
-            return c;
-            }
-        }
-
-    public Card get(int x) {
-        check_index(x);
-        return nodeAt(x).item;
-    }
-
-    public int indexOf(Card x) {
-        /*CardNode<Card> current = front;
-        for (int i = 0; i < this.size; i++) {
-            if (current.item.equals(x))
-                return i;
-            current = current.next;
-        }
-        return -1;*/
-        int index = 0;
-        CardNode<Card> current = front;
-        while (current.next != null && !current.item.equals(x)) {
-            current = current.next;
-            index++;
-        }
-        if (index == this.size -1 && !current.item.equals(x))
-            return -1;
-        return index;
-    }
-
-    public void sort() {
-        if (size <= 1)
-            return;
-        CardLinkedList left = new CardLinkedList();
-        CardLinkedList right = new CardLinkedList();
-        int half = this.size / 2;
-        for (int i = 0; i < half; i++)
-            left.add(this.remove(0));
-        while (this.size > 0)
-            right.add(this.remove(0));
-        left.sort();
-        right.sort();
-        merge(left, right);
-    }
-
-
     private void sort(CardLinkedList x) {
         if (x.size <= 1)
             return;
@@ -195,68 +152,79 @@ public class CardLinkedList implements CardList {
             left.add(x.remove(0));
         while (x.size > 0)
             right.add(x.remove(0));
-        sort(left);
-        sort(right);
+        left.sort();
+        right.sort();
         merge(left, right);
     }
 
-    private void merge(CardLinkedList left, CardLinkedList right) {
-        while (left.size() + right.size() > 0) {
-            if (left.size() == 0)
-                this.add(right.remove(0));
-            else if (right.size() == 0)
-                this.add(left.remove(0));
-            else if (left.front.item.compareTo(right.front.item) < 0)
-                this.add(left.remove(0));
+    private void merge(CardLinkedList start, CardLinkedList end) {
+        while (start.size() + end.size() > 0) {
+            if (start.size() == 0)
+                this.add(end.remove(0));
+            else if (end.size() == 0)
+                this.add(start.remove(0));
+            else if (start.front.item.compareTo(
+                    end.front.item) < 0)
+                this.add(start.remove(0));
             else
-                this.add(right.remove(0));
+                this.add(end.remove(0));
         }
-    }
-
-
-    public void shuffle() {
-        Random rand = new Random();
-        for (int i = 0; i < size * 5; i++) {
-            int a = rand.nextInt(size);
-            int b = rand.nextInt(size);
-            swap(a, b);
-        }
-    }
-
-    public void clear() {
-        front = null;
-        size = 0;
     }
 
     private void swap(int a, int b) {
         CardNode<Card> current = front;
-        CardNode<Card> current2 = front;
+        CardNode<Card> current_2 = front;
         for (int i = 0; i < a; i++)
             current = current.next;
         for (int i = 0; i < b; i++)
-            current2 = current2.next;
+            current_2 = current_2.next;
         Card temp = current.item;
-        current.item = current2.item;
-        current2.item = temp;
+        current.item = current_2.item;
+        current_2.item = temp;
     }
 
 
-    public void reverse() {
-        Stack<Card> stack = new Stack<Card>();
-        while (this.size > 0)
-            stack.push(this.remove(0));
-        while (!(stack.isEmpty()))
-            this.add(stack.pop());
+    /**
+     * @param index the index of the element to return
+     * @return the {@code current} element at the given index
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
+    private CardNode<Card> nodeAt(int index) {
+        check_index_pos(index);
+        CardNode<Card> current = front;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current;
     }
 
-    private void check_index(int index) {
-        if (index < 0 || index > this.size)
-            throw new IndexOutOfBoundsException("Index " + index);
+    private String bound_message(int index) {
+        return "Index: " + index + ", Size: " + this.size;
     }
+
+    // pos = position
+    private void check_index_pos(int index) {
+        if (!(is_position(index)))
+            throw new IndexOutOfBoundsException(bound_message(index));
+    }
+
+    private boolean is_position(int index) {
+        return index >= 0 && index <= this.size;
+    }
+
+    private boolean is_element_index(int index) {
+        return index >= 0 && index < this.size;
+    }
+
+    private void check_element_index(int index) {
+        if (!(is_element_index(index)))
+            throw new IndexOutOfBoundsException(bound_message(index));
+    }
+
 
     private static class CardNode<E> {
-        public E item;
-        public CardNode<E> next;
+       E item;
+       CardNode<E> next;
 
         CardNode(E item) {
             this.item = item;
