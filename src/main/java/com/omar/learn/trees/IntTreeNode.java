198f6d79236c1d4892366ca4b289d1566ea415ed
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-11-24 18:58:12 -0800

    Create tree package

---
+++ src/main/java/com/omar/learn/trees/IntTreeNode.java
@@ -0,0 +1,29 @@
+package com.omar.learn.trees;
+
+/**
+ * @author Omar
+ * @version 11/24/22
+ */
+public class IntTreeNode {
+
+    // class for storing a single node of a binary tree of integers
+
+    public int data;
+    public IntTreeNode left;
+    public IntTreeNode right;
+
+    // post: constructs a leaf node with given data
+    public IntTreeNode(int data) {
+        this(data, null, null);
+    }
+
+    // post: constructs a branch node with
+    // given data, left subtree
+    public IntTreeNode(int data, IntTreeNode left,
+                       IntTreeNode right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
