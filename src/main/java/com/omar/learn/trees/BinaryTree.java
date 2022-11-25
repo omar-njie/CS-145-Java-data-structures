621f2d73dd96a993b4f19823dcfa467697f7a525
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-11-24 18:57:40 -0800

    Create tree package

---
+++ src/main/java/com/omar/learn/trees/BinaryTree.java
@@ -0,0 +1,38 @@
+package com.omar.learn.trees;
+
+/**
+ * @author Omar
+ * @version 11/24/22
+ */
+public class BinaryTree {
+
+    public static void main(String[] args) {
+
+        // Binary Tree:
+            //     1 (root)
+            //    / \
+       // (left) 2   3 (right)
+            //  / \ / \
+     //        4  5 6  7
+
+        // * an empty tree or a root node that refers
+        // to two other trees known as the left subtree and
+        // the right subtree.
+
+        // Branch (Branch Node):
+            // A node that has one or more nonempty subtrees.
+
+        // Leaf (Leaf Node):
+            // A node that has two empty subtrees.
+
+        // Tree Traversal:
+            // The process of visiting each node in a tree exactly once.
+            // 1. Pre-order Traversal
+            // 2. In-order Traversal
+            // 3. Post-order Traversal
+            //              | process root |
+            //  ------------
+            // |
+    }
+
+}
