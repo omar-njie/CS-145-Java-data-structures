cccbacfef48acfa59c0e573fc4de4d2ec1a601da
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-11-28 02:14:07 -0800

    Update
    added `sum()`, `countLevels()` and `countLeaves()`.

--- src/main/java/com/omar/learn/trees/common_tree_operations/common_operations.java
+++ src/main/java/com/omar/learn/trees/common_tree_operations/common_operations.java
@@ -8,7 +8,7 @@ import com.omar.learn.trees.IntTreeNode;
  */
 public class common_operations {
 
-    private IntTreeNode overallRoot;
+    private final IntTreeNode overallRoot;
 
     public common_operations(IntTreeNode overallRoot) {
         this.overallRoot = overallRoot;
@@ -19,7 +19,33 @@ public class common_operations {
     }
 
     private int sum(IntTreeNode root) {
+        if (root == null)
+            return 0;
+        return root.data + sum(root.left) + sum(root.right);
+    }
 
+    public int countLevels() {
+       return countLevels(overallRoot);
+    }
+
+    private int countLevels(IntTreeNode root) {
+        if (root == null)
+            return 0;
+        return 1 + Math.max(countLevels(root.left), countLevels(root.right));
+    }
+
+    public int countLeaves() {
+        return countLeaves(overallRoot);
+    }
+
+    private int countLeaves(IntTreeNode root) {
+        if (root == null) {
+            return 0;
+        } else if (root.left == null && root.right == null) {
+            return 1;
+        } else {
+            return countLeaves(root.left) + countLeaves(root.right);
+        }
     }
 
 }
