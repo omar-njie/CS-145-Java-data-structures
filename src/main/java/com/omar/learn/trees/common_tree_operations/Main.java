38e50a0c41f74b3bf11de815ecaed6341b2523ad
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-11-27 23:46:34 -0800

    Create Tree Common Operations package

---
+++ src/main/java/com/omar/learn/trees/common_tree_operations/Main.java
@@ -0,0 +1,13 @@
+package com.omar.learn.trees.common_tree_operations;
+
+/**
+ * @author Omar
+ * @version 11/27/22
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/com/omar/learn/trees/common_tree_operations/common_operations.java
@@ -0,0 +1,25 @@
+package com.omar.learn.trees.common_tree_operations;
+
+import com.omar.learn.trees.IntTreeNode;
+
+/**
+ * @author Omar
+ * @version 11.27.22
+ */
+public class common_operations {
+
+    private IntTreeNode overallRoot;
+
+    public common_operations(IntTreeNode overallRoot) {
+        this.overallRoot = overallRoot;
+    }
+
+    public int sum() {
+        return sum(overallRoot);
+    }
+
+    private int sum(IntTreeNode root) {
+
+    }
+
+}
