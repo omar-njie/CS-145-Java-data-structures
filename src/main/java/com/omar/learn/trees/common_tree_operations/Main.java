8155cbeaa90b3726cec066039cbb5eb6bf3a9f0e
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-11-28 02:15:02 -0800

    Update Main.java

--- src/main/java/com/omar/learn/trees/common_tree_operations/Main.java
+++ src/main/java/com/omar/learn/trees/common_tree_operations/Main.java
@@ -1,13 +1,24 @@
 package com.omar.learn.trees.common_tree_operations;
 
+import com.omar.learn.trees.IntTreeNode;
+
 /**
  * @author Omar
- * @version 11/27/22
+ * @version 11.27.22
  */
 public class Main {
 
     public static void main(String[] args) {
 
+        common_operations tree = new common_operations(
+                new IntTreeNode(1, new IntTreeNode(2,
+                new IntTreeNode(4),
+                new IntTreeNode(5)),
+                new IntTreeNode(3, new IntTreeNode(6),
+                        new IntTreeNode(7))));
+        System.out.println(tree.sum()); // 28
+        System.out.println(tree.countLevels()); // 3
+        System.out.println(tree.countLeaves()); // 4
     }
 
 }
