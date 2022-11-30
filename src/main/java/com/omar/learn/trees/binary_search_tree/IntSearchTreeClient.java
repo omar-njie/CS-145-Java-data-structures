c152a23866973705acc5a67aa5cc5b34b9784502
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-11-29 20:58:35 -0800

    Update

---
+++ src/main/java/com/omar/learn/trees/binary_search_tree/IntSearchTreeClient.java
@@ -0,0 +1,27 @@
+package com.omar.learn.trees.binary_search_tree;
+
+import java.util.Scanner;
+
+/**
+ * @author Omar
+ * @version 11/29/22
+ */
+public class IntSearchTreeClient {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        IntSearchTree numbers = new IntSearchTree();
+        System.out.print("Next int (0 to quit)? ");
+        int number = in.nextInt();
+        while (number != 0) {
+            numbers.add(number);
+            System.out.print("Next int (0 to quit)? ");
+            number = in.nextInt();
+        }
+        System.out.println();
+        System.out.println("Tree structure:");
+        numbers.printSideways();
+        System.out.println("Sorted list:");
+        numbers.print();
+    }
+}
