7a97b7ffaad3dacb9b347cf4ca95c9b0f76557f4
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-11-29 20:58:44 -0800

    Update

---
+++ src/main/java/com/omar/learn/trees/binary_search_tree/IntSearchTree.java
@@ -0,0 +1,13 @@
+package com.omar.learn.trees.binary_search_tree;
+
+/**
+ * @author Omar
+ * @version 11/29/22
+ */
+public class IntSearchTree {
+
+    // Binary Search Tree:
+        // 1. Properties
+            // - left subtree: values must <= root
+            // - right subtree: values must > root
+}
