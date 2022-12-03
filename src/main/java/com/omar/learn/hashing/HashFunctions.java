dcbd8edf179cec2fc39d407dffbca0866925bd22
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-12-02 20:48:36 -0800

    Update

---
+++ src/main/java/com/omar/learn/hashing/HashFunctions.java
@@ -0,0 +1,17 @@
+package com.omar.learn.hashing;
+
+/**
+ * @author Omar
+ * @version 12/1/22
+ */
+public class HashFunctions {
+
+    // Hash Function:
+        // A method for rapidly mapping between element values
+        // and preferred array indexes at which to store those values
+
+    /*private int hashFunction(int value) {
+        return Math.abs(value) % elementData.length;
+    }*/
+
+}
