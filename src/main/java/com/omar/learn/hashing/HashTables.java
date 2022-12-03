7a6105d7afa82eec7b2583a2f0a5ae2d97173d85
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-12-02 20:49:21 -0800

    Create hashing package

---
+++ src/main/java/com/omar/learn/hashing/HashTables.java
@@ -0,0 +1,13 @@
+package com.omar.learn.hashing;
+
+/**
+ * @author Omar
+ * @version 12/1/22
+ */
+public class HashTables {
+
+    // Hash Table:
+        // An array that stores its elements
+        // in indexes produced by a hash function
+
+}
