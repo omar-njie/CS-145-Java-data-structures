0e7c95d6b5a8ae55160650f8ef4e8b715e5def0f
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-12-02 23:39:09 -0800

    Update TextToCode

--- src/main/java/com/omar/data_structures/assignments/assignment_6/TextToCode.java
+++ src/main/java/com/omar/data_structures/assignments/assignment_6/TextToCode.java
@@ -2,151 +2,215 @@ package com.omar.data_structures.assignments.assignment_6;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
 
 /**
+ * <p>This class is responsible for encoding a text file into a huffman encoded file.
+ * It checks for a file with the given name, if it doesn't exist the program will exit.
+ * It also creates two files, one for the huffman tree and one for the encoded text.
+ * The huffman tree is stored in a file with the extension .code and the encoded text
+ * is stored in a file with the extension .huff.
+ * </p>
+ *
  * @author Omar
- * @version 11.27.22
+ * @version 12.2.22
+ * @see CodeToText
+ *
  */
 public class TextToCode implements Runnable {
 
-    private List<Character> ch;
-    private Map<Character, Integer> map;
-    private PriorityQueue<Node> pq;
-    private Node root;
-    private Map<Character, String> codeMap;
-    private FileUtil fileUtil;
+    private final String CODE_FILE_NAME;
+    private final String HUFF_FILE_NAME;
+    private List<Character> char_list = new ArrayList<>();
+    private Map<Character, Integer> map = new HashMap<>();
+    private PriorityQueue<Node<Character>> pq = new PriorityQueue<>();
+    private Node<Character> root;
+    private PrintWriter code_file;
+    private PrintWriter huff_file;
 
-    public TextToCode() {
-        ch = new ArrayList<>();
+
+    /**
+     *  Creates a TextToCode object, sets the character list, map, priority queue,
+     *  root to null and the variables for some file operations.
+     *
+     *  @param file_name the name of the file to be read
+     */
+    public TextToCode(String file_name) {
+        char_list = new ArrayList<>();
         map = new HashMap<>();
         pq = new PriorityQueue<>();
-        codeMap = new HashMap<>();
+        root = null;
+
+        File file = new File(file_name);
+        CODE_FILE_NAME = file_name.substring(0, file_name.indexOf(".")) + ".code";
+        HUFF_FILE_NAME = file_name.substring(0, file_name.indexOf(".")) + ".huff";
+        code_file = null;
+        huff_file = null;
     }
 
-    public void text_to_code() {
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            pq.add(new Node(entry.getValue(), entry.getKey()));
+
+    /**
+     * <p>This method ask the user for a file name, and check if the entered file
+     * is valid, if not it will ask the user to enter a valid file name.</p>
+     */
+    public static void user_input() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter file name (Should end with .txt): ");
+        String file_name = in.nextLine();
+
+        if (!(file_name.contains("HAMLET"))
+                && !(file_name.contains("hamlet"))
+                || !(file_name.contains(".txt"))) {
+            System.out.println("File name is invalid please try again.");
+            user_input();
         }
-
-        while (pq.size() > 1) {
-            Node left = pq.remove();
-            Node right = pq.remove();
-            Node parent = new Node(left.frequency + right.frequency);
-            parent.left = left;
-            parent.right = right;
-            pq.add(parent);
-        }
-        root = pq.remove();
-
-        Map<Character, String> char_to_str = new HashMap<>();
-        traverse(root, "", char_to_str);
-
     }
 
-    private void traverse(Node root, String s,
-                          Map<Character, String> char_to_str) {
-        if (root == null) {
-            return;
-        }
-        if (root.isLeaf()) {
-            char_to_str.put(root.ch, s);
-            codeMap.put(root.ch, s);
-            return;
-        }
-        traverse(root.left, s + "0", char_to_str);
-        traverse(root.right, s + "1", char_to_str);
-    }
 
-    public void output_code() {
-        String file_name = "FILENAME.code";
-        file_creator(file_name);
-        // read the content of FILENAME.txt
-        // and write the code to FILENAME.code
-        String text = fileUtil.read_file("FILENAME.txt");
-        StringBuilder sb = new StringBuilder();
-    }
-
-    public void file_creator(String file_name) {
+    /**
+     * <p>This method reads the file and stores the characters in a list, and
+     * also check if the file is available or not</p>
+     */
+    public void read_file() {
         try {
-            File f = new File(file_name);
-            if (!f.exists()) {
-                f.createNewFile();
+            FileInputStream fis = new FileInputStream("hamlet.txt");
+            while (fis.available() > 0) {
+                char c = (char) fis.read();
+                char_list.add(c);
+                System.out.print(c);
             }
-        } catch (Exception e) {
+            fis.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
 
+    /**
+     * <p>This method counts the number of occurrences of each character in the
+     * list and stores them in a map.</p>
+     * @see PrintWriter
+     */
+    public void huffManCode() {
+        try {
+            read_file();
+            for (Character ch : char_list)
+                map.put(ch, map.containsKey(ch) ? map.get(ch) + 1 : 1);
+
+            for (Map.Entry<Character, Integer> entry : map.entrySet())
+                pq.add(new Node<>(entry.getValue(), entry.getKey()));
+
+            while (pq.size() > 1) {
+                Node<Character> left = pq.poll();
+                Node<Character> right = pq.poll();
+                assert right != null; // to avoid warning of NullPointerException
+                Node<Character> parent = new Node<>(left.frequency + right.frequency, null);
+                parent.left = left;
+                parent.right = right;
+                pq.add(parent);
+            }
+
+            Map<Character, String> map_2 = new HashMap<>();
+            root = pq.poll();
+            huffManCode(root, "", map_2);
+
+            code_file = new PrintWriter(CODE_FILE_NAME);
+            huff_file = new PrintWriter(HUFF_FILE_NAME);
+
+
+            for (Map.Entry<Character, String> entry : map_2.entrySet()) {
+                code_file.println((int) entry.getKey());
+                code_file.println(entry.getValue());
+            }
+
+            for (Character c : char_list)
+                huff_file.print(map_2.get(c));
+
+            code_file.close();
+            huff_file.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void huffManCode(Node<Character> root, String s,
+                                    Map<Character, String> map_2) {
+        if (root == null) {
+            return;
+        }
+        if (root.isLeaf()) {
+            map_2.put(root.ch, s);
+            return;
+        }
+        huffManCode(root.left, s + "0", map_2);
+        huffManCode(root.right, s + "1", map_2);
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void run() {
-        FileUtil fileUtil = new FileUtil();
-        fileUtil.user_input();
-        fileUtil.file_creator();
-        output_code();
+        user_input();
+        huffManCode();
     }
+
 
     public static void main(String[] args) {
-        TextToCode textToCode = new TextToCode();
-        textToCode.run();
+        TextToCode ttc = new TextToCode("hamlet.txt");
+        ttc.run();
     }
 
-    private class FileUtil {
-        private File file;
-        private String file_name;
-        private Scanner input;
 
-        public FileUtil() {
-            this(null, null, null);
+    private static class Node<E> implements Comparable<Node<E>> {
+
+        E data;
+        Node<E> left;
+        Node<E> right;
+        Integer frequency;
+        Character ch;
+
+        Node(Integer frequency, Character ch) {
+            this.frequency = frequency;
+            this.ch = ch;
         }
 
-        public FileUtil(File file, String file_name, Scanner input) {
-            this.file = file;
-            this.file_name = file_name;
-            this.input = input;
+        Node() {
+            this.data = null;
+            this.frequency = null;
+            this.ch = null;
+
         }
 
-        public void user_input() {
-            input = new Scanner(System.in);
-            System.out.print("Enter file name (Should be FILENAME.txt): ");
-            file_name = input.nextLine();
-
-            if (!(file_name.contains(".txt")
-                    && file_name.contains("FILENAME"))) {
-                System.out.println("File name must be FILENAME and end with \".txt\"");
-                user_input();
-            }
-            read_file();
+        Node(E data) {
+            this(data, null, null);
         }
 
-        public void file_creator() {
-            file = new File(file_name);
-            if (!file.exists()) {
-                try {
-                    file.createNewFile();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+
+        Node(E data, Node<E> left, Node<E> right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
         }
 
-        public void read_file() {
-            try {
-                FileInputStream fis = new FileInputStream(file_name);
-                int i;
-                while ((i = fis.read()) != -1) {
-                    ch.add((char) i);
-                    System.out.print((char) i);
-                }
-                fis.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        public String toString() {
+            return "Character: " + ch + " Frequency: " + frequency;
+        }
 
+        public int compareTo(Node<E> other) {
+            return this.frequency.compareTo(other.frequency);
+        }
+
+
+        public boolean isLeaf() {
+            return left == null && right == null;
         }
     }
+
 }
 
